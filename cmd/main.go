d651b7e76eb18bd852e1ea63e02d2b0dea189216
robert <robert.otherone@gmail.com>
2018-12-28 20:13:52 -0500

    reformatted code and directories.

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,8 @@
+package main 
+
 import (
-    "./routes"
-    "./database"
+    "../api/routes"
+    "../api/database"
 )
 
 
@@ -8,11 +10,8 @@ func main() {
     routes.Load_config()
     // routes.Get_hottest_posts("20")
 
-<<<<<<< HEAD:src/main.go
     routes.TIME_SERIES_INTRADAY("", "", "", "", "", "");
     database.SetupDB();
-=======
     // routes.TIME_SERIES_INTRADAY("", "", "", "", "", "");
->>>>>>> 95915fbce29ed5a295409d5e282e7539eaa0b2f0:api/main.go
     
 }
